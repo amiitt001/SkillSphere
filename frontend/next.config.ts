2f3313f4d42ca1f015dd24075ef2d8bf5472cc9e
amiitt001 <amitverma2k99@gmail.com>
2025-09-19 17:25:58 +0530

    updated

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   // This configuration will ignore TypeScript and ESLint errors during the build
   typescript: {
     // !! WARN !!
