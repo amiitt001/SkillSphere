0a2247439055f346c0c3bec2516c3ad291fa7709
amiitt001 <amitverma2k99@gmail.com>
2025-09-21 13:31:00 +0530

    chore: Remove obsolete backend folder

--- backend/src/routes/recommendationsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const recommendationsController = require('../controllers/recommendationsController');
-
-// Define the POST route for generating recommendations
-router.post('/generate-recommendations', recommendationsController.getRecommendations);
-
-module.exports = router;
